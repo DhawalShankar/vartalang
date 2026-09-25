c167845c7187a76da7624cc1f1dc75389ced0468
dhawalshankar <dhawalmannu@gmail.com>
2026-09-25 23:08:39 +0530

    feat: google-ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 import { DarkModeProvider } from "@/lib/DarkModeContext";
 import { AuthProvider } from "@/lib/AuthContext";
 import Analytics from "@/components/Analytics";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -39,8 +40,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${yatraOne.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${yatraOne.variable} antialiased`}
+      >
 
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-7WM95MNBGL"
           strategy="afterInteractive"
@@ -59,14 +63,33 @@ export default function RootLayout({
             });
           `}
         </Script>
+
+        {/* Google Ads */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18474647156"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18474647156');
+          `}
+        </Script>
+
         <GoogleOAuthProvider clientId={process.env.CLIENT_ID || ""}>
-        <AuthProvider>
-          <DarkModeProvider>
-            <Analytics />
-            {children}
-          </DarkModeProvider>
-        </AuthProvider>
-            </GoogleOAuthProvider>
+          <AuthProvider>
+            <DarkModeProvider>
+              <Analytics />
+              {children}
+            </DarkModeProvider>
+          </AuthProvider>
+        </GoogleOAuthProvider>
+
       </body>
     </html>
   );
